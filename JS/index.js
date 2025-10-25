0ee1b61af6d58464672b88daa67fe7d117b13423
ayushpandey4480@gmamil.com <ayushpandey4480@gmail.com>
2025-10-25 15:21:27 +0530

    js

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+// let a ={name:"John", age:30, city:"New York"};
+// let b = [1,2,3,4,5];
+// console.log(typeof(a));
+// console.log(b);
+// let c  = 10/0;
+// console.log(c );
+
+
+// let a  = 5
+// let b  = 15
+// console.log(a + b );
+// console.log(a % b );
+// console.log(a - b );
+// console.log(a /b );
+// console.log(a * b );
+
+
+// console.log("equal  = ",a <= b );
+
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
+
+
+let age= 18; 
+if (age >=18){
+    console.log("yes");
+}else{
+    console.log("not");
+}
+let ap = age >=18 ? "ha" : "na";
+console.log(ap);
