a18dcde4ccd64e953ce952f32d29a7fbdd8711ba
ayushpandey4480@gmamil.com <ayushpandey4480@gmail.com>
2025-11-10 16:32:39 +0530

    ASYNC / AWAIT

--- Task.js
+++ Task.js
@@ -16,3 +16,66 @@ say(){
 }
 let user= new usr("Manas Rajan", "Odisha", 9744874158)
 user.say()
+
+
+
+// A constructor is a special function inside a class or constructor function that is automatically called when you create a new object using the new keyword.
+// It is used to initialize (assign) values to the object’s properties using the this keyword.
+// When you use new, JavaScript automatically creates a new object and links it to this.
+//➡️ Constructor + new + this = new object with initialized values
+
+
+
+// encapshulation 
+// innheritance
+// polymorphism 
+// abestraction
+
+let copy = ["Ayush", "kumar",["Kishore"]]
+const User = {
+  name: "Ayush",
+  address: { city: "Bhopal", state: "MP" }
+};
+
+// Deep copy using structuredClone
+const deepCopy = {...User}
+
+// let shollow =[...copy]
+// let = ={...obj}
+console.log(deepCopy);
+
+
+
+// distructureing
+
+
+const distructure = {class: "LowerClass", leng:"DownToEarth"}
+const lenguage = distructure.leng
+const classy = distructure.class
+console.log(lenguage);
+console.log(classy);
+console.log(distructure);
+
+
+
+
+
+
+let ages = [1,2,3,4]
+let [one, two, three, four ] = ages
+
+
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+
+
+// destructing is the feature in js that allow to you to get the values form array and define in value in new  seprate varible in short easy way.
+
+
+
+
+async function dp((res,rej)=>{
+    await 
+})
